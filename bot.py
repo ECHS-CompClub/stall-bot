799f341cc6aebe6c96dee69a39862492eab599b6
MelvynKowac <58014757+MelvynKowac@users.noreply.github.com>
2021-01-25 20:46:22 -0600

    added ls and ping command

---
+++ bot.py
@@ -0,0 +1,29 @@
+import discord
+import os
+from discord.ext import commands
+
+client = commands.Bot(command_prefix="~")
+
+def codeblock(string):
+    string = f"```{string}```"
+
+    return string
+
+@client.event
+async def on_ready():
+    print("Bot is Active.")
+
+@client.command()
+async def ping(ctx):
+    await ctx.send(codeblock(f"Pong! {round(client.latency * 1000)}ms"))
+
+@client.command()
+async def ls(ctx):
+    output = ""
+    for file in os.listdir("./"):
+        output += f"{file}\n"
+        
+    await ctx.send(codeblock(output))
+
+
+client.run("ODAzNDQwODc3NDAxNjY5NjYz.YA90oA.5o9n4nsoUK8lY01AeAHdqLTcuvw")
